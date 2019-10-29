5cdc820738e9530cf138f97b97fdd11812eff411
bleaz01 <ridjea@gmail.com>
2019-10-29 16:52:20 +0100

    model + login login controleur

--- model/loginmodel.php
+++ model/loginmodel.php
@@ -1,3 +1,45 @@
 <?php
 //Fichier pour les model de la page login
+
+
+	//connexion à ma DB || include de ma connexion à ma DB
+
+	//Exemple de connexion:
+
+//	$servername = "database";
+//	$usernamedb = "root";
+//	$passworddb = "root";
+//	$dbname = "becode";
+
+//	$conn = mysqli_connect($servername, $usernamedb, $passworddb, $dbname);
+
+
+	// Check connection
+
+
+	// connexion au profile via mon login  
+
+function verify_login($login){
+		
+		//$conn = dbconnect();
+		$sql = "SELECT  * FROM user WHERE username='".$login."' limit 1";
+
+		$result = mysqli_query($conn, $sql);
+		$resultcheck = mysqli_num_rows($result);
+		
+		if ($resultcheck == 1){ // Si il y a bien un username dans la DB qui est  == a l'input
+			if(password_verify($password, $row['password'])){ 
+				return TRUE;
+			}
+			else{
+				return FALSE;
+			}
+		}
+		else {
+			return FALSE;
+		} 
+
+}
+
+
 ?>
