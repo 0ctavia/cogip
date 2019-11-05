e3ed1c3fc86ce8f8156ae9edc22e0c73c6b57e84
bleaz01 <ridjea@gmail.com>
2019-11-05 12:04:27 +0100

    modif

---
+++ view/consultallview.php
@@ -0,0 +1,27 @@
+
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<table border="1">
+			<thead>
+				<th>N°</th>
+				<th>Compagnie</th>
+				<th>Pays</th>
+				<th>Tva</th>
+				<th>type</th>
+				<th>Date d'entrée</th>
+			</thead>
+			<tbody>
+				<?php
+					require("../model/loginmodel.php");
+					$connect = dbconnect();
+					
+
+				?>
+				
+			</tbody>
+		</table>
+	</body>
+</html>
